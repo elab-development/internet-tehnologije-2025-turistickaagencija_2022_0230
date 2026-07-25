7dbf78885264cfe47d2e15db20c3dc3eb5fd1155
Barcho33 <abaralic02@gmail.com>
2026-07-25 18:39:26 +0200

    refactor: delete duplicate

--- Klijent/frontend/src/app/core/services/api-message/login-response.model.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "../../models/user.model";
-
-
-export interface LoginResponse {
-  user: User;
-  token: string;
-}
--- Klijent/frontend/src/app/shared/pipes/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './format-date.pipe';
-export * from './filter-check-in-date.pipe';
-export * from './filter-check-out-date.pipe';
-export * from './format-guests.pipe';
-export * from './format-location.pipe';
